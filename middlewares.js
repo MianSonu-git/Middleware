d11dfca275681237e94aa37af2abfbe0ce835513
MianSonu-git <miantalhan028989@gmail.com>
2022-01-08 08:45:47 +0500

    Express

--- middlewares.js
+++ middlewares.js
@@ -5,13 +5,13 @@ const express = require("express");
 const middlewares = require("./Helper");
 
 const PORT = process.env.PORT || 3000;
-const PORT1 = process.env.PORT || 3001;
+//const PORT1 = process.env.PORT || 3001;
 
 
 const app = express();
-
 //
 app.listen(PORT , ()=>(console.log("Server was runinng on port number" + PORT)));
+
 
 
 
@@ -65,13 +65,75 @@ app.listen(PORT , ()=>(console.log("Server was runinng on port number" + PORT)))
 
     // app.use("/admin",admin);
 //#endregion
-    app.all("/all",(req ,res, next)=>{
-        console.log("used all middlewares");
-        next();
-    });
 
 //#region Method of app in express
+        //var router = express.Router();    
+// app.all('/all',(req ,res, next)=>{
+//     console.log("all method called here" , req.method);
+//     next();
+// });
 
 //#endregion
 //console.log(app.locals.settings.views); // app.locals is variable that hold information about client request
 
+//#region app.Engine method here
+// app.engine('html',require("ejs").renderFile);
+
+// app.get('/all',(req , res)=>{
+//     res.render(__dirname + "/all.html");
+// })
+
+//#endregion
+
+
+//#region app.params method here
+// app.param(['id', 'page','line'], function (req, res, next, value) {
+//     console.log('CALLED ONLY ONCE with', value)
+//     next()
+//   })
+  
+//   app.get('/user/:id/:page/:line', function (req, res, next) {
+//     console.log('although this matches')
+//     next()
+//   })
+  
+//   app.get('/user/:id/:page/:line', function (req, res) {
+//     console.log('and this matches too')
+//     res.end()
+//   })
+//#endregion
+
+
+
+
+//#region call weather api
+// var axios = require("axios").default;
+// var objectHolder = [];
+// var options = {
+//   method: 'GET',
+//   url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+//   params: {
+//     q: 'London,uk',
+//     lat: '0',
+//     lon: '0',
+//     callback: '',
+//     id: '2172797',
+//     lang: 'null',
+//     units: 'metric',
+//     mode: 'json'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+//     'x-rapidapi-key': '4f191f8d8cmsh2be3808d5986143p1cabc6jsn38a2aed72f54'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	objectHolder = response.data;
+//   let {weather,main} = objectHolder;
+//   console.log();
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+//#endregion
